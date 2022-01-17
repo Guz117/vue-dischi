f84338cd7b8965239d26dfc7035cb3549264b33a
Guz117 <cristian.guzman117@gmail.com>
2022-01-17 17:40:06 +0100

    inserimento immagini

---
+++ src/components/Albums.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <div>
+        <img
+      :src="Poster"
+      alt=""
+    >
+    <h2>
+      {{ Title }}
+    </h2>
+    <h3>
+      {{ Author }}
+    </h3>
+    <h4>
+      {{ Year }}
+    </h4>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Albums',
+    props: [
+        'Poster',
+        'Title',
+        'Author',
+        'Year'
+    ]
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,23 +1,41 @@
 <template>
   <main>
-      <div class="row"></div>
+      <div class="row">
+        <Albums 
+            v-for="(album, index) in albums"
+            :key="index"
+            :Poster="album.poster"
+            :Title="album.title"
+            :Author="album.author"
+            :Year="album.year"
+        />
+      </div>
   </main>
 </template>
 
 <script>
-// import axios from 'axios;'
+import axios from 'axios';
+import Albums from './Albums';
 export default {
     name:'Main',
-    // mounted() {
-    //     axios
-    //     .get('https://flynn.boolean.careers/exercises/api/array/music')
-    //     .then((result) => {
-    //         console.log(result);
-    //     })
-    //     .catch ((error) => {
-    //         console.log(error)
-    //     })
-    // }
+    data() {
+        return {
+            albums: null,
+        }
+    },
+    components: {
+        Albums,
+    },
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((result) => {
+            console.log(result);
+            this.albums = result.data.response;
+        })
+        .catch ((error) => {
+            console.log(error)
+        })
+    }
 }
 </script>
 
