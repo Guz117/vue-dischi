6d5d22995542d5485bee85392af6ae83878b0457
Guz117 <cristian.guzman117@gmail.com>
2022-01-17 16:37:14 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+.<template>
+  <header class="p-3">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style>
+    header {
+        background-color: #2E3A46;
+    }
+
+    img {
+        width: 50px;
+        }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+      <div class="row"></div>
+  </main>
+</template>
+
+<script>
+// import axios from 'axios;'
+export default {
+    name:'Main',
+    // mounted() {
+    //     axios
+    //     .get('https://flynn.boolean.careers/exercises/api/array/music')
+    //     .then((result) => {
+    //         console.log(result);
+    //     })
+    //     .catch ((error) => {
+    //         console.log(error)
+    //     })
+    // }
+}
+</script>
+
+<style>
+
+</style>
