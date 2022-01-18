d88ae7912f463ee91dd4c5fdf109e87abdd91d72
Guz117 <cristian.guzman117@gmail.com>
2022-01-18 15:01:01 +0100

    primo test di ricerca

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,13 @@
 <template>
   <main>
+    <div>
+        <Search
+            @doSearch="searchGenre($event)"
+        />
+    </div>      
     <div class="album">
         <Albums 
-            v-for="(album, index) in albums"
+            v-for="(album, index) in filteredAlbum"
             :key="index"
             :Poster="album.poster"
             :Title="album.title"
@@ -16,15 +21,27 @@
 <script>
 import axios from 'axios';
 import Albums from './Albums';
+import Search from './Search';
+
 export default {
     name:'Main',
+    components: {
+        Albums,
+        Search,
+    },
     data() {
         return {
             albums: null,
+            textSearch: '',
         }
     },
-    components: {
-        Albums,
+    computed: {
+        filteredAlbum() {
+            if (this.textSearch === '') {
+                return this.albums;
+            }
+            return this.albums.filter((element) => element.genre.toLowerCase().includes(this.textSearch.toLowerCase()))
+        }
     },
     mounted() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
@@ -35,6 +52,12 @@ export default {
         .catch ((error) => {
             console.log(error)
         })
+    },
+    methods: {
+        searchGenre(text) {
+            console.log(text);
+            this.textSearch = text;
+        }
     }
 }
 </script>
@@ -47,7 +70,7 @@ export default {
         background-color: #1E2D3B;
     }
     .album {
-        width: calc((100% / 2) - 10px);
+        width: 70%;
         margin:0 auto;
         display: flex;
         justify-content: center;
---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="search">
+      <input
+      id="search"
+      v-model="inputText" 
+      @keyup.enter="sendSearch()"
+      type="text"
+      name="search"
+      >
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    data() {
+        return {
+            inputText: '',
+        };
+    },
+    methods: {
+        sendSearch() {
+            this.$emit('doSearch', this.inputText);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
